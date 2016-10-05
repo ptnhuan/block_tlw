8b646afbc1b5ae637c2f8c2a02e56e3ccb3034e5
trinhkhanhduy1 <trinhkhanhduy55@gmail.com>
2016-10-05 20:36:35 +0700

    module 5
    home_5

---
+++ home_5/education_module5/index.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>TODO supply a title</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+        if (!class_exists('lessc')) {
+            include ('libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('less/styles.less', 'css/styles.css');
+        ?> 
+
+        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+        <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+    </head> 
+
+    <body> 
+        <div class="zaro">
+            <div class="contant">
+                <section>
+                    <div class="container">
+                        <div class="sec-header">
+                            <h2>Our Pricing</h2>
+                            <p>Check Our Latest Rates</p>
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="price-box">
+                                    <div class="header">
+                                        <h2>Basic Plan</h2>
+                                        <p>This is the light version</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="rate">
+                                        <h2>50$</h2>
+                                        <p>/Per Month</p>
+                                    </div>
+                                    <div class="price-contant">
+                                        <p>300+ Audio Lesson mp3s</p>
+                                        <p class="striped">50 one-hour long CD’s</p>
+                                        <p><i class="fa fa-check"></i></p>
+                                        <p class="striped">On-line support</p>
+                                    </div>
+                                    <div class="price-footer">
+                                        <a href="#">Sign Up</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="price-box">
+                                    <div class="header">
+                                        <h2>Standart Plan</h2>
+                                        <p>This is the light version</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="rate">
+                                        <h2>100$</h2>
+                                        <p>/Per Month</p>
+                                    </div>
+                                    <div class="price-contant">
+                                        <p>400+ Audio Lesson mp3s</p>
+                                        <p class="striped">400+ Printable Lecture Notes </p>
+                                        <p><i class="fa fa-check"></i></p>
+                                        <p class="striped">On-line support</p>
+                                    </div>
+                                    <div class="price-footer">
+                                        <a href="#">Sign Up</a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-4 col-xs-12">
+                                <div class="price-box">
+                                    <div class="header">
+                                        <h2>Exculsive Plan</h2>
+                                        <p>This is the light version</p>
+                                    </div>
+                                    <div class="clearfix"></div>
+                                    <div class="rate">
+                                        <h2>150$</h2>
+                                        <p>/Per Month</p>
+                                    </div>
+                                    <div class="price-contant">
+                                        <p>Unlimited 1-on-1 Personal Coaching</p>
+                                        <p class="striped">24 hour virtual classroom Q&amp;A</p>
+                                        <p><i class="fa fa-check"></i></p>
+                                        <p class="striped">On-line support</p>
+                                    </div>
+                                    <div class="price-footer">
+                                        <a href="#">Sign Up</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+</body>
+
+
+</html>
