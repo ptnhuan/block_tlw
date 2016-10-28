f594cee6f5177fc4b1dbe6c6b853f6ead79b1f30
trinhkhanhduy1 <trinhkhanhduy55@gmail.com>
2016-10-28 22:35:30 +0700

    module _11

--- home_11/education_module11/index.php
+++ home_11/education_module11/index.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html>
-<html>
 
+<html>
     <head>
-        <title>TODO supply a title</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <?php
-        if (!class_exists('lessc')) {
-            include ('libs/lessc.inc.php');
-        }
-        $less = new lessc;
-        $less->compileFile('less/styles.less', 'css/styles.css');
-        ?> 
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+
 
         <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-        <link href="css/styles.css" rel="stylesheet" type="text/css"/>
         <link href="css/font-awesome.min.css" rel="stylesheet" type="text/css"/>
 
-        <script src="script/bootstrap.min.js" type="text/javascript"></script>
-        <script src="script/jquery-2.1.4.min.js" type="text/javascript"></script>
-        <script src="script/carousel.js" type="text/javascript"></script>
-    </head> 
+        <link href="css/style.css" rel="stylesheet" type="text/css"/>
 
-    <body> 
-
-        <div class="ie-module-11">
+        <?php
+        if (!class_exists('lessc')) {
+            include ('./libs/lessc.inc.php');
+        }
+        $less = new lessc;
+        $less->compileFile('./less/style.less', './css/style.css');
+        ?>
+        <script src="js/jquery-2.1.4.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="js/js.js" type="text/javascript"></script>
+    <body>
+        <div class="all">
             <div class="clearfix"></div>
             <div class="contant"> 
                 <section class="gray-bg">
@@ -38,148 +36,104 @@
                             <span></span>
                         </div>
 
-                        <div class="customNavigation">
-                            <a class="btn prev"><i class="fa fa-angle-left"></i></a>
-                            <a class="btn next"><i class="fa fa-angle-right"></i></a>
-                        </div>
+                        <div class="type-f-slider">
+                            <div class="container">
+                                <div class="row1">
+                                    <a class="left carousel-control next" href="#carousel123" data-slide="prev">
+                                        <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+                                    <a class="right carousel-control prev" href="#carousel123" data-slide="next">
+                                        <i class="fa fa-chevron-left" aria-hidden="true"></i></a>
+                                    <div class="row">
+                                        <div class="col-md-12">
 
-                        <div id="zaro-carousel" class="zaro-carousel" style="display: block; opacity: 1;">
-                            <div class="zaro-wrapper-outer">
-                                <div class="zaro-wrapper" style="width: 7032px; left: 0px; display: block;">
-                                    <div class="zaro-item" style="width: 293px;">
-                                        <div class="course ">
-                                            <div class="thumb">
-                                                <a href="#"><img src="images/course1.jpg" alt=""></a>
-                                                <div class="price"><span>$</span>9</div>
-                                            </div>
-                                            <div class="text">
-                                                <div class="header">
-                                                    <h4>Management</h4>
-                                                    <div class="rating">
-                                                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                                            <div class="carousel carousel-showsixmoveone slide" id="carousel123">
+                                                <div class="carousel-inner">
+                                                    <div class="item active">
+                                                        <div class="  col-md-2">
+                                                            <div class="course ">
+                                                                <div class="thumb">
+                                                                    <a href="#"><img src="images/course1.jpg" alt=""></a>
+                                                                    <div class="price"><span>$</span>9</div>
+                                                                </div>
+                                                                <div class="text">
+                                                                    <div class="header">
+                                                                        <h4>Management</h4>
+                                                                        <div class="rating">
+                                                                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="course-name">
+                                                                        <p>Build a Business Plan</p>
+                                                                        <span>$800</span>
+                                                                    </div>
+                                                                    <div class="course-detail-btn">
+                                                                        <a href="#">Subscribe</a>
+                                                                        <a href="#">Detail</a>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                                <div class="course-name">
-                                                    <p>Build a Business Plan</p>
-                                                    <span>$800</span>
-                                                </div>
-                                                <div class="course-detail-btn">
-                                                    <a href="#">Subscribe</a>
-                                                    <a href="#">Detail</a>
+                                                    <div class="item">
+                                                        <div class="  col-md-2">
+                                                            <div class="course ">
+                                                                <div class="thumb">
+                                                                    <a href="#"><img src="images/course1.jpg" alt=""></a>
+                                                                    <div class="price"><span>$</span>9</div>
+                                                                </div>
+                                                                <div class="text">
+                                                                    <div class="header">
+                                                                        <h4>Management</h4>
+                                                                        <div class="rating">
+                                                                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="course-name">
+                                                                        <p>Build a Business Plan</p>
+                                                                        <span>$800</span>
+                                                                    </div>
+                                                                    <div class="course-detail-btn">
+                                                                        <a href="#">Subscribe</a>
+                                                                        <a href="#">Detail</a>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <div class="item">
+                                                        <div class="  col-md-2">
+                                                            <div class="course ">
+                                                                <div class="thumb">
+                                                                    <a href="#"><img src="images/course1.jpg" alt=""></a>
+                                                                    <div class="price"><span>$</span>9</div>
+                                                                </div>
+                                                                <div class="text">
+                                                                    <div class="header">
+                                                                        <h4>Management</h4>
+                                                                        <div class="rating">
+                                                                            <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
+                                                                        </div>
+                                                                    </div>
+                                                                    <div class="course-name">
+                                                                        <p>Build a Business Plan</p>
+                                                                        <span>$800</span>
+                                                                    </div>
+                                                                    <div class="course-detail-btn">
+                                                                        <a href="#">Subscribe</a>
+                                                                        <a href="#">Detail</a>
+                                                                    </div>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    </div>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
-                                    <div class="zaro-item" style="width: 293px;">
-                                        <div class="course">
-                                            <div class="thumb">
-                                                <a href="#"><img src="images/course1.jpg" alt=""></a>
-                                                <div class="price"><span>$</span>9</div>
-                                            </div>
-                                            <div class="text">
-                                                <div class="header">
-                                                    <h4>Management</h4>
-                                                    <div class="rating">
-                                                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                                                    </div>
-                                                </div>
-                                                <div class="course-name">
-                                                    <p>Build a Business Plan</p>
-                                                    <span>$800</span>
-                                                </div>
-                                                <div class="course-detail-btn">
-                                                    <a href="#">Subscribe</a>
-                                                    <a href="#">Detail</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="zaro-item" style="width: 293px;">
-                                        <div class="course">
-                                            <div class="thumb">
-                                                <a href="#"><img src="images/course1.jpg" alt=""></a>
-                                                <div class="price"><span>$</span>9</div>
-                                            </div>
-                                            <div class="text">
-                                                <div class="header">
-                                                    <h4>Management</h4>
-                                                    <div class="rating">
-                                                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                                                    </div>
-                                                </div>
-                                                <div class="course-name">
-                                                    <p>Build a Business Plan</p>
-                                                    <span>$800</span>
-                                                </div>
-                                                <div class="course-detail-btn">
-                                                    <a href="#">Subscribe</a>
-                                                    <a href="#">Detail</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="zaro-item" style="width: 293px;">
-                                        <div class="course">
-                                            <div class="thumb">
-                                                <a href="#"><img src="images/course1.jpg" alt=""></a>
-                                                <div class="price"><span>$</span>9</div>
-                                            </div>
-                                            <div class="text">
-                                                <div class="header">
-                                                    <h4>Management</h4>
-                                                    <div class="rating">
-                                                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                                                    </div>
-                                                </div>
-                                                <div class="course-name">
-                                                    <p>Build a Business Plan</p>
-                                                    <span>$800</span>
-                                                </div>
-                                                <div class="course-detail-btn">
-                                                    <a href="#">Subscribe</a>
-                                                    <a href="#">Detail</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="zaro-item" style="width: 293px;">
-                                        <div class="course">
-                                            <div class="thumb">
-                                                <a href="#"><img src="images/course1.jpg" alt=""></a>
-                                                <div class="price"><span>$</span>9</div>
-                                            </div>
-                                            <div class="text">
-                                                <div class="header">
-                                                    <h4>Management</h4>
-                                                    <div class="rating">
-                                                        <span>☆</span><span>☆</span><span>☆</span><span>☆</span><span>☆</span>
-                                                    </div>
-                                                </div>
-                                                <div class="course-name">
-                                                    <p>Build a Business Plan</p>
-                                                    <span>$800</span>
-                                                </div>
-                                                <div class="course-detail-btn">
-                                                    <a href="#">Subscribe</a>
-                                                    <a href="#">Detail</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                </div>
-                            </div>
-
-                            <div class="zaro-controls clickable">
-                                <div class="zaro-pagination">
-                                    <div class="zaro-page active"><span class=""></span></div>
-                                    <div class="zaro-page"><span class=""></span></div>
-                                    <div class="zaro-page"><span class=""></span></div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </section>
             </div>
         </div>
     </body>
